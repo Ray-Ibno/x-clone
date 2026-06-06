478d6d906b3294806c0dcf66312939fc4ec98974
Alradenn Ibno <alradennibno@gmail.com>
2026-06-06 16:15:09 +0800

    fix(spinner): fix for spinner animation not centered
    the spinner animation in the PublicRoute component needed a 'w-screen' class to be centered

--- frontend/src/PublicRoute.tsx
+++ frontend/src/PublicRoute.tsx
@@ -7,7 +7,7 @@ const PublicRoute = () => {
 
   if (isLoading) {
     return (
-      <div className="h-screen flex justify-center items-center">
+      <div className="h-screen w-screen flex justify-center items-center">
         <LoadingSpinner size="lg" />
       </div>
     )
