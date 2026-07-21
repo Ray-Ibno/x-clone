ffdfff70c180c87f6469963e3b050d55e0de36b0
Alradenn <alradennibno@gmail.com>
2026-07-21 10:22:24 +0800

    refractor(backend): move cookie baking to the controllers

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,77 +1,64 @@
-import { generateAccessToken, generateRefreshToken, jwt } from '../utils/generateTokens.js'
+import { generateAccessToken, generateRefreshToken } from '../utils/generateTokens.js'
 import * as authService from '../services/auth.service.js'
 import AppError from '../errors/AppError.js'
 import redis from '../config/redis.js'
-
-export const signup = async (req, res) => {
-  const newUser = await authService.register(
-    req.body.username,
-    req.body.fullName,
-    req.body.email,
-    req.body.password,
-  )
-
-  const accessToken = generateAccessToken(newUser._id)
-  await generateRefreshToken(newUser._id, res)
-
-  res.status(201).json({
-    accessToken,
-    username: newUser.username,
-    fullName: newUser.fullName,
-    password: newUser.password,
-    email: newUser.email,
-    followers: newUser.followers,
-    following: newUser.following,
-    profileImg: newUser.profileImg,
-    coverImg: newUser.coverImg,
-    bio: newUser.bio,
-    link: newUser.link,
-  })
-}
-
-export const login = async (req, res) => {
-  const user = await authService.signIn(req.body.email, req.body.password)
-
-  const accessToken = generateAccessToken(user.id)
-  await generateRefreshToken(user.id, res)
-
-  res.status(200).json({
-    accessToken,
-    username: user.username,
-    fullName: user.fullName,
-    email: user.email,
-    followers: user.followers,
-    following: user.following,
-    profileImg: user.profileImg,
-    coverImg: user.coverImg,
-    bio: user.bio,
-    link: user.link,
-  })
-}
-
-export const logout = async (req, res) => {
-  res.cookie('jwt', '', { maxAge: 0 })
-  res.status(200).json({ message: 'successfully logged out' })
-}
 
 export const getUser = async (req, res) => {
   const user = await authService.findUser(req.user._id)
   res.status(200).json(user)
 }
 
+export const signup = async (req, res) => {
+  const { newUser, newAccessToken, newRefreshToken } = await authService.register(req.body)
+
+  res.cookie('jwt', newRefreshToken, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV !== 'development',
+  })
+
+  res.status(201).json({ accessToken: newAccessToken, ...newUser })
+}
+
+export const login = async (req, res) => {
+  const { userObj, newAccessToken, newRefreshToken } = await authService.signIn(req.body)
+
+  res.cookie('jwt', newRefreshToken, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV !== 'development',
+  })
+
+  res.status(200).json({ accessToken: newAccessToken, ...userObj })
+}
+
+export const logout = async (req, res) => {
+  if (!req.cookies || !req.cookies.jwt) throw new AppError('No jwt cookies found', 401)
+  if (!req.user || !req.user._id) throw new AppError('No user id found', 401)
+
+  const userId = req.user._id
+  const refreshToken = req.cookies.jwt
+
+  await authService.deleteTokenAndSession(userId, refreshToken)
+
+  res.cookie('jwt', '', { maxAge: 0 })
+  res.status(200).json({ message: 'successfully logged out' })
+}
+
 export const refresh = async (req, res) => {
-  const cookies = req.cookies
-  if (!cookies?.jwt) throw new AppError('No jwt cookies found', 401)
-  const refreshToken = cookies.jwt
+  if (!req.cookies || !req.cookies.jwt) throw new AppError('No jwt cookies found', 401)
 
-  const verifiedRefreshToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+  const refreshToken = req.cookies.jwt
+  const { newAccessToken, newRefreshToken } = await authService.handleRefreshToken(refreshToken)
 
-  const storedRefreshToken = await redis.sismember(
-    `x-clone-session:${verifiedRefreshToken.userId}`,
-    refreshToken,
-  )
-  if (!storedRefreshToken) throw new AppError('Expired token', 403)
+  res.cookie('jwt', newRefreshToken, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV !== 'development',
+  })
 
-  const accessToken = generateAccessToken(verifiedRefreshToken.userId)
-  res.json({ accessToken })
+  res.status(200).json({ accessToken: newAccessToken })
 }
--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import redis from '../config/redis.js'
+import User from '../models/user.model.js'
 
 const generateAccessToken = (userId) => {
   const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
@@ -8,21 +9,14 @@ const generateAccessToken = (userId) => {
   return accessToken
 }
 
-const generateRefreshToken = async (userId, res) => {
+const generateRefreshToken = (userId) => {
   const key = `x-clone-session:${userId}`
 
-  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+  const newRefreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
   })
 
-  await redis.sadd(key, refreshToken)
-
-  res.cookie('jwt', refreshToken, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: 'strict',
-    secure: process.env.NODE_ENV !== 'development',
-  })
+  return newRefreshToken
 }
 
-export { generateAccessToken, generateRefreshToken, jwt }
+export { generateAccessToken, generateRefreshToken }
