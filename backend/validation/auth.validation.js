502fbf9fd071fe1f23d8697d3d930cf2aa2dfdc1
Alradenn <alradennibno@gmail.com>
2026-07-21 10:15:25 +0800

    fix(backend): add missing validation error messages

--- backend/validation/auth.validation.js
+++ backend/validation/auth.validation.js
@@ -4,22 +4,22 @@ export const signupSchema = z
   .object({
     email: z.email('Please provide a valid email'),
     username: z
-      .string()
+      .string('Please provide a username')
       .min(3, 'Username must be at least 3 charaters long')
       .max(12, 'Username should not exceed 12 characters long')
       .regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores'),
-    fullName: z.string(),
+    fullName: z.string('Please provide a full name'),
     password: z
-      .string()
+      .string('Please provide a password')
       .min(8, 'Password must be at least 8 characters long')
       .regex(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
         'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
       ),
-    passwordRepeat: z.string(),
+    passwordRepeat: z.string('Please provide a repeat password'),
   })
   .refine((data) => data.password === data.passwordRepeat, {
-    message: 'Password do not match',
+    message: 'Passwords do not match',
     path: ['passwordRepeat'],
   })
 
